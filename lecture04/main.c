5b90154f3f1c878dc8a522cbd2e14c24bb2ca640
siwoo1010 <siwoo5737@naver.com>
2025-04-08 14:33:18 +0900

    siwoo1010

---
+++ lecture04/main.c
@@ -0,0 +1,43 @@
+int title_screen()
+{
+	puts("********************");
+	puts("   PHS's lovestory *");
+	puts("********************");
+	return 0;
+}
+
+int title_screen()
+{  
+	puts("********************");
+	puts("   PHS's lovestory *");
+	puts("********************");
+	return 0;
+}
+int title_screen()
+{
+	puts("********************");
+	puts("   PHs's lovestory *");
+	puts("********************");
+	return 0;
+}
+int title_screen()
+{
+	puts("********************");
+	puts("   PHS's lovestory *");
+	puts("********************");
+	return 0;
+}
+int title_screen()
+{
+	puts("********************");
+	puts("   PHS's lovestory *");
+	puts("********************");
+}
+
+int main()
+{       
+	title_screen();
+	return 0;
+}
+	 
+
